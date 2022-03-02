77831599b7f462da9508cf29460225cbea715f49
NickDavis17 <063815@swcsd.us>
2022-03-02 13:05:22 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person person1 = new Person("Nick", "063815@swcsd.us");
+        person1.display();
+        Business business1 = new Business("Nick","063815@swcsd.us", "6143548378");
+        business1.display();
+    }
+}
